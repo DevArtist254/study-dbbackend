32a6298853ba3d193df2095be4b19254a5d0ed21
DevArtist254 <kevinwasonga415@outlook.com>
2023-09-16 10:49:02 +0300

    app set up

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('Hello world');
+const dotenv = require('dotenv');
+const app = require('./app');
+
+dotenv.config({path: './config.env'});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
